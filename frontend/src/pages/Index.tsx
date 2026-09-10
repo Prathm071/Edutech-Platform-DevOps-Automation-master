6f61c98b657207d817c615e6907776e34a84ca2a
Pratham <prathmeshpatel3214@gmail.com>
2026-09-10 17:00:58 +0530

    Change index

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -29,7 +29,7 @@ const Index = () => {
       ]
     },
     {
-      title: "Data Science & Analytics",
+      title: "DS",
       description: "Transform data into actionable insights",
       icon: ChartBar,
       slug: "data-science",
